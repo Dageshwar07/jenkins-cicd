e798bb25efdafa5fdedf0d75e9efe7490d04edb8
Dageshwar07 <dageshwardasmanikpuri996@gmail.com>
2025-04-26 14:06:56 +0530

    res changed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/test",(req,res)=>{
-    res.json("server started successfully with jenkins pipeline")
+    res.json("server started successfully with jenkins")
 })
 
 app.use(router);
